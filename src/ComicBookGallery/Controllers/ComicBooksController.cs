d233820b51a184d6ee8d9c5f77a3a088f3d8ec83
KHolden <threedogyarn@gmail.com>
2017-10-15 13:19:49 -0500

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -15,17 +15,12 @@ namespace ComicBookGallery.Controllers
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
-                DescriptionHtml = "<p> Final issue!  Witness the final hour of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</string> </p>",
+                DescriptionHtml = "<p>Final issue!</p>",
                 Artists = new Artist[]
                 {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
+                    new Artist() { Name = "Dan", Role = "Script" }
                 }
             };
-
             
             return View(comicBook);
         }
